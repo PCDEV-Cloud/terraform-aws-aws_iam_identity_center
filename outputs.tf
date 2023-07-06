3cd7d04ea3a099cb037e8b1707a9aea08e16d227
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-07-06 08:14:40 +0200

    Add outputs and update examples

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
-# output "permission_set_arns" {
-#   value       = merge({ for i in aws_ssoadmin_permission_set.predefined : i.name => i.arn }, { for i in aws_ssoadmin_permission_set.custom : i.name => i.arn })
-#   description = "A map of permission set ARNs."
-# }
+output "permission_set_arns" {
+  value = merge(
+    { for i, k in aws_ssoadmin_permission_set.predefined : i => k.arn },
+    { for i, k in aws_ssoadmin_permission_set.custom : i => k.arn }
+  )
+  description = "A map of permission set ARNs."
+}
+
+output "permission_set_ids" {
+  value = merge(
+    { for i, k in aws_ssoadmin_permission_set.predefined : i => k.id },
+    { for i, k in aws_ssoadmin_permission_set.custom : i => k.id }
+  )
+  description = "A map of permission set IDs."
+}
